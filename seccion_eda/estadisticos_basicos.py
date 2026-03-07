98f806d60e9c1b679678155927e216e0b976db26
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-07 12:35:30 +0000

    cambios basicos

--- seccion_eda/estadisticos_basicos.py
+++ seccion_eda/estadisticos_basicos.py
@@ -6,10 +6,13 @@ import seaborn as sns
 df = pd.read_csv("bike_dataset_hour.csv")
 
 st.markdown("## Datos Crudos del dataset")
+st.dataframe(df.head(500))
 
 st.markdown("## Datos sobre el dataset")
+st.dataframe(df.describe().T)
 
 st.markdown("## Estadísticos Básicos")
+st.dataframe(df[['dteday', 'temp', 'windspeed', 'hum','cnt']].describe().T[['count', 'std', 'mean']])
 
 st.markdown("## Distribución de las variables")
 
