e3b7cba8b17a195fce32702b54478ff851fd3222
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-07 11:33:05 +0000

    avances-menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-st.title("Mi primera App")
-st.balloons()
+pg_intro = st.Page("intro.py", title="Introducción")
+
+# Paginas EDA
+pg_eda_intro = st.Page("seccion_eda/intro_eda.py", title="Primeros Pasos")
+
+
+navigation_env = st.navigation(
+    {
+        "": [pg_intro],
+        "EDA": [pg_eda_intro]
+
+    }
+)
+
+navigation_env.run()
