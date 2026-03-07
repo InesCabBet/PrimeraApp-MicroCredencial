1409c943dd8db779bd2919f877ff2b7eeb89ec0d
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-07 12:33:50 +0000

    esqueleto

--- seccion_eda/estadisticos_basicos.py
+++ seccion_eda/estadisticos_basicos.py
@@ -3,3 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+df = pd.read_csv("bike_dataset_hour.csv")
+
+st.markdown("## Datos Crudos del dataset")
+
+st.markdown("## Datos sobre el dataset")
+
+st.markdown("## Estadísticos Básicos")
+
+st.markdown("## Distribución de las variables")
+
+st.markdown("## Matriz de correlación")
+
